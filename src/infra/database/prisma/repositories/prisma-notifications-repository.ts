0512d01f23428a4d38844c94a58fec0f5a20aa78
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2022-12-19 03:24:44 -0300

    cancel notification, count recipient notifications

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -7,6 +7,13 @@ import { PrismaService } from '../prisma.service'
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
+  async findById(notificationId: string): Promise<Notification> {
+    throw new Error('Method not implemented.')
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    throw new Error('Method not implemented.')
+  }
 
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification)
@@ -14,5 +21,9 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
     await this.prismaService.notification.create({
       data: raw,
     })
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.')
   }
 }
